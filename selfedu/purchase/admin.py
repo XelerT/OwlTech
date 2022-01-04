3056ade2c2d39db33b70a108b6b0bde5499fe03b
Alex Taranov <57453100+XelerT@users.noreply.github.com>
2022-01-04 21:10:21 +0300

    Add files via upload

--- selfedu/purchase/admin.py
+++ selfedu/purchase/admin.py
@@ -9,6 +9,16 @@ class PurchaseListAdmin(admin.ModelAdmin):
     readonly_fields = ['user']
 
     class Meta:
-        model = FriendList
+        model = PurchaseList
 
 admin.site.register(PurchaseList, PurchaseListAdmin)
+
+class PurchaseRequestAdmin(admin.ModelAdmin):
+    list_filter = ['buyer', 'item']
+    list_display = ['buyer', 'item']
+    search_fields = ['buyer__username', 'buyer__email', 'item__product_name']
+
+    class Meta:
+        modal = PurchaseRequest
+
+admin.site.register(PurchaseRequest, PurchaseRequestAdmin)
